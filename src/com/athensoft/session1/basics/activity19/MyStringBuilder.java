a058889cbcbe78e9211530cc65ef36c20243e8e3
Athens <Athens@WS-ATHENS>
2019-03-20 13:59:54 -0400

    edited stringbuilder

--- src/com/athensoft/session1/basics/activity19/MyStringBuilder.java
+++ src/com/athensoft/session1/basics/activity19/MyStringBuilder.java
@@ -8,6 +8,14 @@ package com.athensoft.session1.basics.activity19;
 public class MyStringBuilder {
 
 	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("aaa");
+		sb.append("bbb");
+		sb.append("cccc");
+		sb.append("ddd");
+		
+		System.out.println(sb.toString());
 
 	}
 
