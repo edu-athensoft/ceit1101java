4981c4e62f314d29996130267257293044b99cdc
Athens <Athens@WS-ATHENS>
2019-03-27 15:20:23 -0400

    added MyIf

---
+++ src/com/athensoft/session1/basics/activity20/MyIf.java
@@ -0,0 +1,15 @@
+package com.athensoft.session1.basics.activity20;
+
+/**
+ * Activity 20 - if structure and branching
+ * @author Athens
+ *
+ */
+public class MyIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/athensoft/session1/basics/activity20/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * activity 20 - if structure and branching
+ * @author Athens
+ *
+ */
+package com.athensoft.session1.basics.activity20;
